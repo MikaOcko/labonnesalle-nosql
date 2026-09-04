18afc9dac86271ae8d5355d49b7415d278292bc9
Kimberly Moreau <moreau.magali.kimberly@gmail.com>
2026-09-04 17:00:59 +0200

    add documentation

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,18 @@ const app = express();
 const port = 3000;
 
 // ---------- Logic ----------
+// Database Check
 connectDatabase();
-app.get("/", (request, response) => {
-    response.send("hello two");
-});
 
 // Middleware pour parser le JSON
 app.use(express.json());
-
+//Router
 app.use("/api", roomRouter);
-
+// Health check
+app.get("/", (request, response) => {
+    response.send("hello two");
+});
+// Start srver
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`);
 });
